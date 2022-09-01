d4b554604de8b5725dbf83a601627f366c5984d8
twismdx <twism@live.com>
2022-09-01 13:41:20 +0930

    apitest3

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   const getScores = async () => {
     const response = await fetch (API_SCORES, {
       headers: { 
-        "Ocp-Apim-Subscription-Key": a5a933d50f7b40928d1e0c0612903033,
+        "Ocp-Apim-Subscription-Key": "a5a933d50f7b40928d1e0c0612903033",
 
       }});	
     const data = await response.json();
